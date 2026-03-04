5a85399af09f6a071c8bf2acf30b1e3e877bba8e
mmmharius <mariuspapin@rcmm.fr>
2026-03-04 02:25:43 +0100

    nginx sh print error

--- srcs/requirements/nginx/tools/nginx.sh
+++ srcs/requirements/nginx/tools/nginx.sh
@@ -5,8 +5,10 @@ if [ ! -f /etc/nginx/ssl/nginx.crt ]; then
     openssl req -x509 -nodes -days 365 -newkey rsa:2048 \
         -keyout /etc/nginx/ssl/nginx.key \
         -out /etc/nginx/ssl/nginx.crt \
-        -subj "/C=FR/ST=IDF/L=Paris/O=42/OU=student/CN=mpapin.42.fr"
+        -subj "/C=FR/ST=IDF/L=Paris/O=42/OU=student/CN=${DOMAIN_NAME}"
 fi
 
-envsubst '${DOMAIN_NAME}' < /etc/nginx/sites-available/default > /etc/nginx/sites-enabled/default
-exec nginx -g "daemon off;"
+envsubst '${DOMAIN_NAME}' < /etc/nginx/sites-available/default > /tmp/nginx_default
+cp /tmp/nginx_default /etc/nginx/sites-enabled/default
+
+nginx -t && exec nginx -g "daemon off;"
